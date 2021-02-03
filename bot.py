cba1276a784567c5027ab57176be67122d944ef8
JetDesktop <jethro0610@outlook.com>
2021-02-02 17:16:47 -0800

    Add balance saving

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import atexit
 import asyncio
 import os
 import json
@@ -7,8 +8,11 @@ from discord.ext import commands, tasks
 from jipesoclasses import SmashSet
 from jipesoclasses import Bet
 
-with open("config.json") as json_data_file:
+with open('config.json') as json_data_file:
     config = json.load(json_data_file)
+
+with open('jipeso.json') as json_data_file:
+    jipesoUsers = json.load(json_data_file)
 
 smashggKey = config['smashggKey']
 eventId = config['eventId']
@@ -20,13 +24,31 @@ bot = commands.Bot(command_prefix='$')
 smashSets = dict()
 bets = None
 
+def ensure_jipesoUser_exists(jipesoUserId):
+    if not jipesoUserId in jipesoUsers:
+        jipesoUsers[str(jipesoUserId)] = {'balance' : 100}
+
+def get_balance(jipesoUserId):
+    ensure_jipesoUser_exists(jipesoUserId)
+    return jipesoUsers[str(jipesoUserId)]['balance']
+
+def set_balance(jipesoUserId, amount):
+    ensure_jipesoUser_exists(jipesoUserId)
+    jipesoUsers[str(jipesoUserId)]['balance'] = amount
+
+@tasks.loop(seconds=60.0)
+async def save_jipesos():
+    with open('jipeso.json', 'w') as json_data_file:
+        json.dump(jipesoUsers, json_data_file)
+    print("Saved.")
+
 async def calculate_bets(betsChannel, finishedSet):
     totalBetAmount = 0.0
     winnerBetAmount = 0.0
     for betKey in finishedSet.bets:
         bet = finishedSet.bets[betKey]
         totalBetAmount += bet.amount
-        if(bet.predictionId == finishedSet.winner):
+        if bet.predictionId == finishedSet.winner:
             winnerBetAmount += bet.amount
         
     await betsChannel.send('%s won the set. %d Jipesos were side bet' % (finishedSet.players[finishedSet.winner], totalBetAmount))
@@ -38,26 +60,16 @@ async def calculate_bets(betsChannel, finishedSet):
         bet = finishedSet.bets[betKey]
         percentOfPot = bet.amount / winnerBetAmount
         earnings = totalBetAmount * percentOfPot
-        await betsChannel.send('<@%s> earned %d Jipesos(%d%% of pot) in bettings' % (betKey, earnings, percentOfPot * 100))
         
-@commands.command()
-async def betIDs(ctx, setId, predictionId, amount):
-    if not int(setId) in smashSets:
-        await ctx.channel.send('<@%s> Invalid set ID' % (ctx.author.id))
-        return
-
-    setToBet = smashSets[int(setId)]
-    if not ctx.author.id in setToBet.bets:
-        if int(predictionId) in setToBet.players:
-            setToBet.bets[ctx.author.id] = Bet(int(predictionId), int(amount))
-            await ctx.channel.send('<@%s> has placed a %d Jipeso bet on set %d' % (ctx.author.id, int(amount), int(setId)))
-        else:
-            await ctx.channel.send('<@%s> Invalid player ID' % (ctx.author.id))
-    else:
-        await ctx.channel.send('<@%s> You already placed a bet on this set' % (ctx.author.id))
+        beterBalance = get_balance(betKey) + earnings
+        set_balance(betKey, beterBalance)
+        await betsChannel.send('<@%s> earned %d Jipesos(%d%% of pot) in bettings. Their balance is now %d' % (betKey, earnings, percentOfPot * 100, beterBalance))
 
 @commands.command()
 async def bet(ctx, predictionName, amount):
+    amount = float(amount)
+    beterBalance = get_balance(ctx.author.id)
+    
     setToBet = None
     predictionInt = 0
     opponentInt = 0
@@ -73,17 +85,28 @@ async def bet(ctx, predictionName, amount):
             counter += 1
         
     if setToBet == None:
-        await ctx.channel.send('<@%s> Couldn\'t find match/player to bet on' % (ctx.author.id, int(amount), predictionName))
+        await ctx.channel.send('<@%s> Couldn\'t find match/player to bet on' % (ctx.author.id))
         return
 
     opponentInt = 1 - predictionInt
-    playerKeyList = list(smashSet.players)
+    playerKeyList = list(setToBet.players)
+    opponent = setToBet.players[playerKeyList[opponentInt]]
     
     if not ctx.author.id in setToBet.bets:
-        setToBet.bets[ctx.author.id] = Bet(int(predictionId), int(amount))
-        await ctx.channel.send('<@%s> has placed a %d Jipeso bet on %s\'s set vs. %s' % (ctx.author.id, int(amount), predictionName, smashSet.players[playerKeyList[opponentInt]]))
+        if beterBalance < amount:
+            await ctx.channel.send('<@%s> Your bet is more than your account balance (%d Jipesos)' % (ctx.author.id, beterBalance))
+            return
+    
+        setToBet.bets[ctx.author.id] = Bet(int(predictionId), amount)
+        set_balance(ctx.author.id, beterBalance - amount)
+        await ctx.channel.send('<@%s> has placed a %d Jipeso bet on %s\'s set vs. %s. Their balance is now %d Jipesos' % (ctx.author.id, amount, predictionName, opponent, get_balance(ctx.author.id)))
     else:
         await ctx.channel.send('<@%s> You already placed a bet on this set' % (ctx.author.id))
+
+@commands.command()
+async def balance(ctx):
+    ensure_jipesoUser_exists(ctx.author.id)
+    await ctx.channel.send('<@%s> Your balance is %d Jipesos' % (ctx.author.id, get_balance(ctx.author.id)))
 
 @tasks.loop(seconds=5.0)
 async def update_sets():
@@ -104,14 +127,14 @@ async def update_sets():
             await betsChannel.send(endString)
             await calculate_bets(betsChannel, smashSet)
             smashSet.ended = True
-
+    
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     update_sets.start()
-    
-bot.add_command(betIDs)
-bot.add_command(bet)
-bot.run(discordKey)
+    save_jipesos.start()
 
-print("end")
+atexit.register(save_jipesos)
+bot.add_command(bet)
+bot.add_command(balance)
+bot.run(discordKey)
